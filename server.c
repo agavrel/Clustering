dfbd3a732d582109377678f99e947f938945dc67
Antonin GAVREL <angavrel@e3r4p15.42.fr>
2017-07-02 02:47:45 +0200

    [ADD] Adding thread handling and sub functions

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
+  // inspired from http://www.linuxhowtos.org/C_C++/socket.htm tutorial
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h> // adding thread
 
 void error(const char *msg)
 {
@@ -14,47 +16,78 @@ void error(const char *msg)
     exit(1);
 }
 
+typedef struct	s_client_data
+{
+	int					newsockfd;
+	struct sockaddr_in	serv_addr;
+}				t_client_data;
+
+void *connect_to_server(void *client_data)
+{
+	t_client_data		*data;
+	int					n;
+	char				buffer[256];
+
+	data = (t_client_data *)client_data;
+	while (42)
+	{
+		// 5) Send and receive data
+		bzero(buffer, 256);
+		if ((n = read(data->newsockfd, buffer, 255)) < 0)
+			error("ERROR reading from soc0ket");
+		printf("From %d: %s\n", data->serv_addr.sin_port, buffer);
+		if (!strcmp(buffer, "exit\n"))
+		{
+			close(data->newsockfd);
+			break ;
+		}
+		if ((n = write(data->newsockfd, buffer, strlen(buffer)) < 0))
+			error("ERROR writing to socket");
+	}
+	pthread_exit(NULL); // ferme le thread
+}
+
+static void init_server_data(struct sockaddr_in	*serv_addr, int portno, int *sockfd)
+{
+	// Get serveur addresss informations
+	bzero((char *) serv_addr, sizeof(* serv_addr));
+	serv_addr->sin_family = AF_INET;
+	serv_addr->sin_addr.s_addr = INADDR_ANY;
+	serv_addr->sin_port = htons(portno);
+	// 2) Bind the socket to an address using the bind() system call. For a server socket on the Internet, an address consists of a port number on the host machine.
+	if (bind(*sockfd, (struct sockaddr *) serv_addr, sizeof(* serv_addr)) < 0)
+		error("ERROR on binding");
+	// 3) Listen for connections with the listen() system call
+	listen(*sockfd, 5); // 5 client max waiting to connect
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
+	int 				sockfd;
+	struct sockaddr_in 	cli_addr;
+	pthread_t			thr;
+	t_client_data		client_data;
+	socklen_t 			clilen;
+
+     if (argc < 2)
+	 {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
+// 1) Create a socket for the client with the socket() system call
+     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) // should be recreated if client disconnect
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-	listen(sockfd,5);
+	init_server_data(&client_data.serv_addr, atoi(argv[1]), &sockfd);
 	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 	while (42)
 	{
-        if (newsockfd < 0)
-             error("ERROR on accept");
-        bzero(buffer,256);
-        if ((n = read(newsockfd,buffer,255)) < 0)
-			error("ERROR reading from socket");
-        printf("From %d: %s\n", newsockfd, buffer);
-        if ((n = write(newsockfd,"I got your message",18)) < 0)
-    		error("ERROR writing to socket");
-		if (!strcmp(buffer, "exit\n"))
-		{
-			close(newsockfd);
-	        close(sockfd);
-			break ;
-		}
+	// 4) Accept a connection with the accept() system call. This call typically blocks until a client connects with the server.
+		if ((client_data.newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0) // create the client fd and should be in the loop
+			 error("ERROR on accept");
+	// steps 4) and 5)
+		pthread_create(&thr, NULL, connect_to_server, &client_data);
+		pthread_join(thr, NULL);
 	}
-    return 0;
+	close(sockfd);
+    return (0);
 }
